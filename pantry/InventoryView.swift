9ef8a1f3969d8e89f0c069862fa3a52aeada3305
Joris <joris.bayer@protonmail.com>
2020-08-29 20:35:56 +0200

    photo in inventory list

--- pantry/InventoryView.swift
+++ pantry/InventoryView.swift
@@ -15,18 +15,54 @@ struct InventoryView: View {
     @FetchRequest(entity: Product.entity(), sortDescriptors: [])
     var products: FetchedResults<Product>
 
+    let currentDate = Date()
+
     var body: some View {
 
         List {
             ForEach(products) { product in
-                if product.expiryDate != nil {
-                    Text("\(product.expiryDate!)")
-                } else {
-                    Text("no expiry date")
+                HStack {
+
+                    self.photo(product)
+
+                    Text(self.expiryText(product))
+
                 }
+
+            }
+        }
+    }
+
+    private func photo(_ product: Product) -> Image {
+        if let photoData = product.photo {
+            if let uiImage = UIImage(data: photoData) {
+
+                return Image(uiImage: uiImage)
             }
         }
 
+        return Image(systemName: "photo")
+    }
+
+    private func expiryText(_ product: Product) -> String {
+        if let expiryDate = product.expiryDate {
+
+            let diff = expiryDate.timeIntervalSince(self.currentDate)
+            let numDays = (diff / 3600).rounded()
+
+            if numDays == 0 {
+
+                return "expires today"
+            } else if numDays < 0 {
+
+                return "expired \(numDays) ago"
+            }
+
+            return "expires in \(numDays) days"
+        } else {
+
+            return "no expiry date"
+        }
     }
 }
 
