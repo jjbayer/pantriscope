5f545f9e7ae47197cb30b7e6dc4408500b8ce43f
Joris <joris.bayer@protonmail.com>
2020-10-15 16:39:56 +0200

    Pimp expiry date screen

--- pantry/Widgets/ScanExpiryDate.swift
+++ pantry/Widgets/ScanExpiryDate.swift
@@ -13,95 +13,31 @@ import AVFoundation
 
 struct ScanExpiryDate: View {
 
-    // I would prefer to bind to Core Data object directly, but I haven't figured
-    // out how to handle optional bindings yet
-    @State private var hasExpiryDate = true
     @State private var expiryDate = Date()
-    @State private var currentConfidence = 0.0  // Parser confidence
+    @State private var confidence = 0.0  // Parser confidence
+    @State private var dateWasSelected = false
 
     @Binding var scanProductMode: ScanProductMode
     @Binding var statusMessage: StatusMessage
 
     @Environment(\.managedObjectContext) var managedObjectContext
 
+    let cornerRadius = CGFloat(10.0)
+
     var body: some View {
         VStack {
-
-            HStack {
-                Button(action: {
-                    scanProductMode = .takeSnapshot
-                }) {
-                    Text("< back")
-                }
-                .background(Color.white)
-                Spacer()
-            }
-
-
-
+            backButton.padding(EdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5))
             Spacer()
-
-            if hasExpiryDate {
-                // TODO: set confidence to 2.0 when user selects date
-                DatePicker("Expiry Date", selection: $expiryDate, displayedComponents: .date)
-                .labelsHidden()
-                    .background(Color.white)
-
-            }
-
-
-            Toggle(isOn: $hasExpiryDate) {
-                Text("product has expiry date")
-            }
-
-            Button(action: {
-                let product = Product(context: self.managedObjectContext)
-                product.id = UUID()
-                if let data = Camera.instance.captureHandler.data {
-                    product.photo = data
-                    detectText(imageData: data, onSuccess: { text in
-                        product.detectedText = text
-
-                        // Save, because async
-                        do {
-                            try self.managedObjectContext.save()
-                            self.statusMessage.info("Product saved.")
-                        } catch {
-                            self.statusMessage.error(error.localizedDescription)
-                        }
-                    })
-                }
-                if self.hasExpiryDate {
-                    product.expiryDate = self.expiryDate
-                }
-
-                do {
-                    try self.managedObjectContext.save()
-                    self.statusMessage.info("Product saved.")
-                } catch {
-                    self.statusMessage.error(error.localizedDescription)
-                }
-                scanProductMode = .takeSnapshot
-            }) {
-                HStack {
-                    Image(systemName: "plus.circle")
-                    Text("Add Product")
-                }
-            }
-            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 50)
-            .foregroundColor(.white)
-            .background(Color.green)
-            .cornerRadius(20)
+            controlPanel
         }
-        .padding()
         .onAppear {
             print("expdate appear")
             Camera.instance.onFrame { frame in
                 detectExpiryDate(sampleBuffer: frame, onSuccess: { parsed in
-                    if parsed.confidence > currentConfidence {
+                    if parsed.confidence > confidence {
                         print("set parsed date \(parsed.date) with confidence \(parsed.confidence)")
                         expiryDate = parsed.date
-                        currentConfidence = parsed.confidence
+                        confidence = parsed.confidence
                     }
                 })
             }
@@ -112,5 +48,88 @@ struct ScanExpiryDate: View {
         }
     }
 
+    var backButton: some View {
+        HStack {
+            Button(action: {
+                scanProductMode = .takeSnapshot
+            }) {
+                HStack { Image(systemName: "chevron.left"); Text("Back") }.padding()
+            }
+            Spacer()
+        }
+    }
+
+    var controlPanel: some View {
+        VStack {
+            datePanel
+            saveButton
+        }
+        .padding()
+    }
+
+    var labelText: String {
+        if dateWasSelected {
+            return "Expiry date:"
+        } else {
+            return "Detecting expiry date..."
+        }
+    }
+
+    var datePanel: some View {
+        VStack {
+            DatePicker(labelText, selection: $expiryDate, displayedComponents: .date)
+                .onTapGesture {
+                    confidence = 0.0
+                }
+                .onChange(of: expiryDate, perform: { _ in
+                    print("date changed ")
+                    dateWasSelected = true
+                })
+                .padding()
+                .background(Color.white)
+                .cornerRadius(cornerRadius)
+        }
+    }
+
+    var saveButton: some View {
+        // TODO: disable button when not detected
+        Button(action: {
+            let product = Product(context: self.managedObjectContext)
+            product.id = UUID()
+            if let data = Camera.instance.captureHandler.data {
+                product.photo = data
+                detectText(imageData: data, onSuccess: { text in
+                    product.detectedText = text
+
+                    // Save, because async
+                    do {
+                        try self.managedObjectContext.save()
+                        self.statusMessage.info("Product saved.")
+                    } catch {
+                        self.statusMessage.error(error.localizedDescription)
+                    }
+                })
+            }
+            product.expiryDate = self.expiryDate
+
+            do {
+                try self.managedObjectContext.save()
+                self.statusMessage.info("Product saved.")
+            } catch {
+                self.statusMessage.error(error.localizedDescription)
+            }
+            scanProductMode = .takeSnapshot
+        }) {
+            HStack {
+                Image(systemName: "plus.circle")
+                Text("Add Product")
+            }
+        }
+        .disabled(!dateWasSelected)
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 50)
+        .foregroundColor(.white)
+        .background(dateWasSelected ? Color.green : Color.gray)
+        .cornerRadius(cornerRadius)
+    }
 }
 
