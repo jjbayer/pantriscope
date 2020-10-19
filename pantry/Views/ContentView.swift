1ff9ba972d52fe68a39123d72ea7e13ccea4eec0
Joris <joris.bayer@protonmail.com>
2020-10-19 17:36:05 +0200

    Change tab bar icon

--- pantry/Views/ContentView.swift
+++ pantry/Views/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
 
             ScanProduct()
                 .tabItem {
-                    Image(systemName: "photo")
+                    Image(systemName: "camera")
                     Text("Scan")
                 }
                 .tag(TabID.takeSnapshot)
