55d99bdf8511c93289b05f2b970825f8f10ca11b
Joris <joris.bayer@protonmail.com>
2020-10-22 15:16:32 +0200

    Viewfinders for camera views

--- pantry/Views/ScanProduct.swift
+++ pantry/Views/ScanProduct.swift
@@ -20,30 +20,64 @@ struct ScanProduct: View {
 
     @State var scanProductMode = ScanProductMode.takeSnapshot
 
+    let relViewFinderWidth: CGFloat = 0.9
+
     var body: some View {
-        ZStack {
-            CameraView()
-                .scaledToFill()
-                .layoutPriority(-1) // https://stackoverflow.com/questions/58290963/clip-image-to-square-in-swiftui
-            VStack {
+        GeometryReader { geometry in
+            ZStack {
+                CameraView()
+                    .scaledToFill()
+                    .layoutPriority(-1) // https://stackoverflow.com/questions/58290963/clip-image-to-square-in-swiftui
 
-                StatusMessageView(statusMessage: $statusMessage)
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(App.Colors.note, lineWidth: 4)
+                    .frame(
+                        width: relViewFinderWidth * geometry.size.width,
+                        height: relViewFinderHeight * geometry.size.width, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
 
-                Spacer()
+                VStack {
 
-                if scanProductMode == .takeSnapshot {
-                    TakeSnapshotView(scanProductMode: $scanProductMode)
-                } else {
-                    ScanExpiryDate(scanProductMode: $scanProductMode, statusMessage: $statusMessage)
-                        
+                    StatusMessageView(statusMessage: $statusMessage)
+
+                    Spacer()
+
+                    if scanProductMode == .takeSnapshot {
+                        TakeSnapshotView(scanProductMode: $scanProductMode)
+                    } else {
+                        ScanExpiryDate(scanProductMode: $scanProductMode, statusMessage: $statusMessage)
+
+                    }
                 }
             }
         }
+    }
+
+    private var relViewFinderHeight: CGFloat {
+        let aspectRatio = (scanProductMode == .takeSnapshot ? 1.0 : 1.0/3)
+        return CGFloat(aspectRatio) * relViewFinderWidth
     }
 }
 
 struct ScanProduct_Previews: PreviewProvider {
     static var previews: some View {
-        ScanProduct()
+        GeometryReader { geometry in
+            ZStack {
+
+                Rectangle().scaledToFill().layoutPriority(-1) // represents camera
+
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(App.Colors.note, lineWidth: 4)
+                    .frame(width: 0.9 * geometry.size.width, height: 0.3 * geometry.size.width, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+
+                VStack {
+                    StatusMessageView(statusMessage: .constant(StatusMessage()))
+                    Spacer()
+
+
+                    Text("Bottom content").foregroundColor(.pink)
+                }
+
+            }
+        }
     }
 }
